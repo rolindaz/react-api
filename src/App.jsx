7c63dcb795c7fdb8d056f5fc344851890f17c2be
rolindaz <rolinda.zamboni@gmail.com>
2025-07-22 14:53:35 +0200

    Add initial folders and files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+/*
+Consegna:
+
+MILESTONE 1:
+Al caricamento dell'applicazione, recuperiamo la lista degli attori e delle attrici dalle API e stampiamoli in console.
+
+MILESTONE 2:
+Prepariamo una card per ciascun attore/attrice, mostrandone le seguenti informazioni:
+- nome
+- anno nascita
+- nazionalità
+- biografia
+- immagine
+- riconoscimenti
+- immagine
+
+MILESTONE 3:
+Mostriamo in pagina una card per ciascun attore, con grafica a piacimento!
+*/
+
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
